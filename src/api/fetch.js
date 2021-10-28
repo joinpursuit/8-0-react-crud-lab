dc827086793329b878671b0d7d48508f61ad6a07
Wes Reid <bwreid@gmail.com>
2021-10-28 13:55:28 -0700

    Initial project setup

---
+++ src/api/fetch.js
@@ -0,0 +1,10 @@
+const { REACT_APP_API_BASE_URL } = process.env;
+const URL = `${REACT_APP_API_BASE_URL}`;
+
+export function getAllMovies() {
+  return fetch(`${URL}/movies`).then((response) => response.json());
+}
+
+export function getAllShows() {
+  return fetch(`${URL}/shows`).then((response) => response.json());
+}
