030c781c3db9165daa82c44d557a383627fa10f6
Mason Mei <masonmei@pursuit.org>
2022-06-06 20:38:50 -0500

    index, show pages done

--- src/components/movies/MoviesIndex.js
+++ src/components/movies/MoviesIndex.js
@@ -1,7 +1,12 @@
 import React from "react";
+import MovieListing from "./MovieListing";
+import Movie from "./Movie";
+import './MoviesIndex.css';
+// import Error from "../common/Error";
 
 // Helper functions
 import { getAllMovies } from "../../api/fetch";
+import { Switch, Route } from "react-router-dom";
 
 class MoviesIndex extends React.Component {
   constructor(props) {
@@ -22,7 +27,24 @@ class MoviesIndex extends React.Component {
   }
 
   render() {
-    return <p>Movie List</p>;
+    return (
+      <Switch>
+        {/* The below route is for when we click on a specific movie link! */}
+        <Route path="/movies/:id"> 
+          <Movie movies={this.state.movies} />
+        </Route>
+      <section className="movies-index-wrapper">
+        <h2>All movies</h2>
+        {/* <!-- movieListing components --> */}
+        <section className="movies-index">
+          {this.state.movies.map((movie) => {
+            // console.log(movie)
+            return <MovieListing movie={ movie } key={ movie.id }/>
+          })}
+        </section>
+      </section>
+      </Switch>
+    )
   }
 }
 
--- src/components/shows/ShowsIndex.js
+++ src/components/shows/ShowsIndex.js
@@ -1,7 +1,12 @@
 import React from "react";
+import ShowListing from "./ShowListing";
+import Show from "./Show";
+import './ShowsIndex.css';
+// import Error from "../common/Error";
 
 // Helper functions
 import { getAllShows } from "../../api/fetch";
+import { Switch, Route } from "react-router-dom";
 
 class ShowsIndex extends React.Component {
   constructor(props) {
@@ -22,7 +27,24 @@ class ShowsIndex extends React.Component {
   }
 
   render() {
-    return <p>Shows List</p>;
+    return (
+      <Switch>
+        {/* The below route is for when we click on a specific show link! */}
+        <Route path="/shows/:id"> 
+          <Show shows={this.state.shows} />
+        </Route>
+      <section className="shows-index-wrapper">
+        <h2>All Shows</h2>
+        {/* <!-- ShowListing components --> */}
+        <section className="shows-index">
+          {this.state.shows.map((show) => {
+            // console.log(show)
+            return <ShowListing show={ show } key={ show.id }/>
+          })}
+        </section>
+      </section>
+      </Switch>
+    )
   }
 }
 
