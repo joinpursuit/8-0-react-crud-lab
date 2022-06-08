0233a0b2115e01dba602f58955c9e47039a8b981
Zane <zaneahmed4@gmail.com>
2022-06-07 22:03:20 -0400

    lab

---
+++ src/components/common/Error.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Error() {
+  return (
+    <section className="error">
+    <p>Something went wrong! Please try again later.</p>
+  </section>
+  )
+}
--- src/components/shows/ShowsIndex.js
+++ src/components/shows/ShowsIndex.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Error from "../common/Error";
+import Show from "../shows/Show"
+
 
 // Helper functions
-import { getAllShows } from "../../api/fetch";
+import { getAllShows, deleteShow} from "../../api/fetch";
+import ShowListing from "./ShowListing";
+import { Switch, Route, withRouter } from "react-router-dom";
 
 class ShowsIndex extends React.Component {
   constructor(props) {
@@ -21,9 +26,45 @@ class ShowsIndex extends React.Component {
       });
   }
 
+  handleDelete = (event) => {
+    const value = event.target.value
+    try{
+
+      deleteShow(value)
+      .then(()=>{
+        const index = this.state.shows.findIndex(show=> show.id === value)
+        const updatedShows = [...this.state.shows]
+        updatedShows.splice(index, 1)
+        this.setState({
+          shows:updatedShows
+        })
+        //navigate("/shows") is new way
+        this.props.history.push("/shows")
+      })
+
+
+    } catch(err){
+      console.log(err)
+    }
+  }
+
   render() {
-    return <p>Shows List</p>;
+    return (
+      <Switch>
+        <Route path="/shows/:id">
+          <Show shows={this.state.shows} handleDelete={this.handleDelete}/>
+        </Route>
+        <section className="shows-index-wrapper">
+          <h2>All Shows</h2>
+          <section className="shows-index">
+            {this.state.shows.map((show) => {
+              return <ShowListing show={show} key={show.id} />;
+            })}
+          </section>
+        </section>
+      </Switch>
+    );
   }
 }
 
-export default ShowsIndex;
+export default withRouter(ShowsIndex) ;
