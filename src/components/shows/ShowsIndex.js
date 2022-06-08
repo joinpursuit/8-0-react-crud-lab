03ee4f403a1e6ef47ab4948c3b793b6278aff88e
Khaleed Uddin <uddin1786@gmail.com>
2022-06-08 19:02:32 -0400

    Add files via upload

--- src/components/shows/ShowsIndex.js
+++ src/components/shows/ShowsIndex.js
@@ -1,7 +1,12 @@
 import React from "react";
-
+import Error from "../common/Error";
 // Helper functions
 import { getAllShows } from "../../api/fetch";
+import ShowListing from "./ShowListing";
+import Show from "./Show";
+import { Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
+
 
 class ShowsIndex extends React.Component {
   constructor(props) {
@@ -22,7 +27,35 @@ class ShowsIndex extends React.Component {
   }
 
   render() {
-    return <p>Shows List</p>;
+    return (
+      <div>
+
+      <Switch>
+          <Route path="/shows/:id">
+            <Show shows={this.state.shows} />
+          </Route>
+        
+        <Route>
+        <section className="shows-index-wrapper">
+          <h2>All Shows</h2>
+          <section className="shows-index">
+            {this.state.shows.map((show) => {
+              return <ShowListing
+                show={show}
+                id={show.id}
+                title={show.title}
+                description={show.description}
+                duration={show.duration}
+                listedIn={show.listedIn}
+                rating={show.rating}
+                key={show.id} />
+            })}
+          </section>
+        </section>
+        </Route>
+      </Switch>
+      </div>
+    )
   }
 }
 
