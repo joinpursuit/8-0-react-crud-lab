3d4e38c8a20b447c71746f933604d9c5295636ca
King Ankoue <thierryankoue@pursuit.com>
2022-07-04 14:07:32 -0400

    crud

--- src/components/movies/MoviesIndex.js
+++ src/components/movies/MoviesIndex.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Error from '../common/Error'
+import Movie from './Movie'
 
-// Helper functions
-import { getAllMovies } from "../../api/fetch";
+import { deleteMovie,getAllMovies } from "../../api/fetch";
+import MovieListings from './MovieListings'
+import { Switch, Route, withRouter } from 'react-router-dom'
+import './MoviesIndex.css'
+
+
 
 class MoviesIndex extends React.Component {
   constructor(props) {
@@ -20,10 +26,42 @@ class MoviesIndex extends React.Component {
         this.setState({ loadingError: true });
       });
   }
-
+  handleDelete = (event) => {
+    const { id } = event.target.value
+    try {
+      deleteMovie(id)
+        .then(() => {
+          const index = this.state.movies.findIndex(movie => movie.id === id)
+          const updateMovies = [...this.state.movies]
+          updateMovies.splice(index, 1)
+          this.setState({ movies: updateMovies })
+        })
+      this.props.history.push('/movies')
+    } catch (err) {
+      console.log(err)
+      this.setState({ loadingError: true })
+    }
+  }
   render() {
-    return <p>Movie List</p>;
+    if (this.state.loadingError) {
+      return <Error/>
+    }
+    return(
+     <Switch>
+        <Route path="/movies/:id">
+          <Movie movies={this.state.movies} handleDelete={this.handleDelete} />
+        </Route>
+        <section className="shows-index-wrapper">
+          <h2>All Movies</h2>
+          <section className="shows-index">
+            {this.state.movies.map((movie) => {
+              return <MovieListings movie={movie} key={movie.id} />;
+            })}
+          </section>
+        </section>
+      </Switch>
+    );
   }
 }
 
-export default MoviesIndex;
+export default withRouter(MoviesIndex);
--- src/components/shows/ShowsIndex.js
+++ src/components/shows/ShowsIndex.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React from 'react';
+import Error from '../common/Error';
 
-// Helper functions
-import { getAllShows } from "../../api/fetch";
+
+import { deleteShow, getAllShows } from '../../api/fetch';
+import ShowListings from './ShowListings';
+import { Switch, Route, withRouter } from "react-router-dom"
+import "./ShowsIndex.css"
+import Show from './Show'
+
 
 class ShowsIndex extends React.Component {
   constructor(props) {
@@ -20,10 +26,48 @@ class ShowsIndex extends React.Component {
         this.setState({ loadingError: true });
       });
   }
+  handleDelete = (event) => {
+    const id = event.target.value
+    try {
+      deleteShow(id)
+        .then(() => {
+          const index = this.state.shows.findIndex(show => show.id === id)
+          const updatedShows = [...this.state.shows]
+          updatedShows.splice(index, 1)
+          this.setState({shows:updatedShows})
+        
+        })
+      
+      this.props.history.push('/shows')
+      
+      
+    } catch (err) {
+      console.log(err)
+      this.setState({loadingError: true})
+    }
+  }
 
   render() {
-    return <p>Shows List</p>;
+    if (this.state.loadingError) {
+      return <Error />
+    }
+    return (
+      <Switch>
+        <Route path="/shows/:id">
+          <Show shows={this.state.shows} handleDelete={this.handleDelete} />
+        </Route>
+        <section className="shows-index-wrapper">
+          <h2>All Shows</h2>
+          <section className="shows-index">
+            {this.state.shows.map((show) => {
+              return <ShowListings show={show} key={show.id} />;
+            })}
+          </section>
+        </section>
+        
+      </Switch>
+    );
   }
 }
-
-export default ShowsIndex;
+           
+export default withRouter(ShowsIndex);
