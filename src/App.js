8d8a5ecac902bab109c3be302477d5024235b26f
Lennie Nurse <lnursej@gmail.com>
2022-11-02 18:47:46 -0400

    "update"

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Show from "./components/shows/Show"
 import ShowsEditForm from "./components/shows/ShowsEditForm"
 import ShowsIndex from "./components/shows/ShowsIndex"
 import ShowsNewForm from "./components/shows/ShowsNewForm"
+import MoviesEditForm from "./components/movies/MoviesEditForm"
+import MoviesIndex from "./components/movies/MoviesIndex"
+import MoviesNewForm from "./components/movies/MoviesNewForm"
 
 function App() {
   return (
