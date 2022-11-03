024b3dc9b3860d4079df2a1d9ad2eb2959f364f2
Kylie-Lang1 <kylielang@pursuit.org>
2022-11-03 14:18:53 -0400

    Updated Shows, ShowsListing, ShowsIndex and MoviesIndex. Created MoviesListing

---
+++ src/components/movies/MovieListing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MovieListing() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default MovieListing;
--- src/components/movies/MoviesIndex.js
+++ src/components/movies/MoviesIndex.js
@@ -1,3 +1,48 @@
+import { useEffect, useState } from "react";
+import { getAllMovies } from "../../api/fetch";
+import ErrorMessage from "../errors/ErrorMessage";
+import MovieListing from "./MovieListing";
+
 export default function MoviesIndex() {
-  return <p>Movie List</p>;
+  const [movies, setMovies] = useState([])
+  const [loadingError, setLoadingError] = useState(false);
+
+  useEffect(() => {
+    getAllMovies()
+      .then(res => {
+        setMovies(res)
+      })
+  })
+
+  return (
+    <div>
+      {
+        loadingError 
+        ?
+        (<ErrorMessage />)
+        :
+        (
+        <section>
+          <h2>All Movies</h2>
+          <button>Add a new movie</button>
+          <br/>
+          <label htmlFor="searchMovie" >
+            Search Movies:
+            <input
+              type="text"
+              id="searchMovie"
+            />
+          </label>
+          <section className="movies-index">
+            {
+              movies.map((movie) => {
+                return <MovieListing />
+              })
+            }
+          </section>
+        </section>
+        )
+      }
+    </div>
+  )
 }
