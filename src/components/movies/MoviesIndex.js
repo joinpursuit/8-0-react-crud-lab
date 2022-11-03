dba15cef9bff7b029fc25ca399c0e0d0fbc4b1fc
Lennie Nurse <lnursej@gmail.com>
2022-11-03 16:55:01 -0400

    "update"

--- src/components/movies/MoviesIndex.js
+++ src/components/movies/MoviesIndex.js
@@ -1,3 +1,42 @@
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import MoviesListing from "react"
+
+import { getAllMovies } from "../../api/fetch"
+
 export default function MoviesIndex() {
-  return <p>Movie List</p>;
+  const [movies, setMovies] = useState([])
+  const [searchMovies, setSearchMovies] = useState([])
+
+  function handleTextChange(e) {
+    const movieTitle = e.target.value
+
+    setMovies()
+    setSearchMovies(movieTitle)
+  }
+  return (
+    <div>
+      <section className="movies-index-wrapper">
+        <p>Movie List</p>
+        <button>
+          <Link to="/movies/new">Add New Movie</Link>
+        </button>
+        <br></br>
+        <label htmlFor="SearchMovies">
+          Search Movies:
+          <input
+            type="text"
+            value={searchMovies}
+            id="searchMovies"
+            onChange={handleTextChange}
+          />
+        </label>
+        <section className="movies-index">
+          {movies.map((movie) => {
+            return <MoviesListing movie={movie} key={movie.id} />
+          })}
+        </section>
+      </section>
+    </div>
+  )
 }
