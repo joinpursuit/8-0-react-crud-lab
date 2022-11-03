327eccd62376e49551514e1e732fede460e138db
Daniel-Mazzilli <danmazzilli@pursuit.org>
2022-11-03 17:11:35 -0400

    starting movies section

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,4 +1,5 @@
 // Shows
+const URL = process.env.REACT_APP_API_BASE_URL;
 
 // Create
 export function createShow(show) {
@@ -7,17 +8,18 @@ export function createShow(show) {
 
 // Delete
 export function destroyShow(id) {
-  return;
+  const options = { method: "DELETE" };
+  return fetch(`${URL}/shows/${id}`, options);
 }
 
 // Index/Get all
 export function getAllShows() {
-  return;
+  return fetch(`${URL}/shows`).then((res) => res.json());
 }
 
 // Show/Get one
 export function getOneShow(id) {
-  return;
+  return fetch(`${URL}/shows/${id}`).then((res) => res.json());
 }
 
 // Update
