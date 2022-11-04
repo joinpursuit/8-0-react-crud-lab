6b1f9056d6a322dc239174858582fb4c08d06a7a
bowersjuan <bowersjuan@gmail.com>
2022-11-03 21:25:33 -0400

    Create Movie.js and add new Route to App

---
+++ src/components/movies/movie.js
@@ -0,0 +1,81 @@
+import { useState, useEffect } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
+
+import "./Movie.css";
+
+import ErrorMessage from "../errors/ErrorMessage";
+import { getOneMovie, destroyMovie } from "../../api/fetch";
+
+function Show() {
+  const [show, setShow] = useState({});
+  const [loadingError, setLoadingError] = useState(false);
+
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  function handleDelete(id) {
+    destroyShow(id)
+      .then(() => navigate("/shows"))
+      .catch((error) => {
+        console.error(error);
+        setLoadingError(true);
+      });
+  }
+
+  useEffect(() => {
+    getOneShow(id)
+      .then((response) => {
+        setShow(response);
+        if (Object.keys(response).length === 0) {
+          setLoadingError(true);
+        } else setLoadingError(false);
+      })
+      .catch((error) => {
+        setLoadingError(true);
+      });
+  }, [id]);
+
+  return (
+    <section className="shows-show-wrapper">
+      <h2>{show.title}</h2>
+      <section className="shows-show">
+        {loadingError ? (
+          <ErrorMessage />
+        ) : (
+          <>
+            <aside>
+              <p>
+                <span>Duration:</span> {show.duration}
+              </p>
+              <p>
+                <span>Listed Categories:</span> {show.listedIn}
+              </p>
+              <p>
+                <span>Country:</span> {show.country}
+              </p>
+              <p>
+                <span>Rating:</span> {show.rating}
+              </p>
+              <p>
+                <span>Date Added:</span> {show.dateAdded}
+              </p>
+            </aside>
+            <article>
+              <p>{show.description}</p>
+            </article>
+            <aside>
+              <button className="delete" onClick={() => handleDelete(show.id)}>
+                Remove show
+              </button>
+              <Link to={`/shows/${id}/edit`}>
+                <button>Edit</button>
+              </Link>
+            </aside>
+          </>
+        )}
+      </section>
+    </section>
+  );
+}
+
+export default Show;
