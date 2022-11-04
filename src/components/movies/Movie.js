58ca266b99d8b69fc67292ed70e0bf19a3a50bea
Lennie Nurse <lnursej@gmail.com>
2022-11-04 10:13:04 -0400

    "update"

--- src/components/movies/Movie.js
+++ src/components/movies/Movie.js
@@ -1,7 +1,26 @@
 import React from "react"
 import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
 
-function Movie(prop) {
+import { getOneMovie } from "../../api/fetch"
+import ErrorMessage from "../errors/ErrorMessage"
+
+function Movie() {
+  const [movie, setMovie] = useState({})
+  const [loadingError, setLoadingError] = useState(false)
+
+  const { id } = useParams()
+
+  useEffect(() => {
+    getOneMovie(id)
+      .then((res) => {
+        setMovie(res)
+        if (Object.keys(res).length === 0) setLoadingError(true)
+        else setLoadingError(false)
+      })
+      .catch((err) => setLoadingError(true))
+  }, [id])
+
   return <div></div>
 }
 
