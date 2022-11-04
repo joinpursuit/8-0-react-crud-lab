14217419ab2dc7bb18314e56493714edf130e6ef
Lennie Nurse <lnursej@gmail.com>
2022-11-04 09:21:59 -0400

    "update"

--- src/components/movies/MoviesIndex.js
+++ src/components/movies/MoviesIndex.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
-import MoviesListing from "react"
+import MoviesListing from "./MoviesListing"
 
 import { getAllMovies } from "../../api/fetch"
 
