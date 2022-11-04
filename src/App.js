0399315d66893fb5eb3ff9bbb041a7bee491c875
bowersjuan <bowersjuan@gmail.com>
2022-11-03 22:00:26 -0400

    Create and update MoviesEditForm component and create Route path for /movies/id/edit path

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/common/Footer";
 import Home from "./components/home/Home";
 import Nav from "./components/common/Nav";
+
+// Shows Components
 import Show from "./components/shows/Show";
 import ShowsEditForm from "./components/shows/ShowsEditForm";
 import ShowsIndex from "./components/shows/ShowsIndex";
 import ShowsNewForm from "./components/shows/ShowsNewForm";
-import MoviesIndex from "./components/movies/MoviesIndex";
+
+// Movies Components
+
 import Movie from "./components/movies/Movie";
+import MoviesEditForm from "./components/movies/MoviesEditForm";
+import MoviesIndex from "./components/movies/MoviesIndex";
 import MoviesNewForm from "./components/movies/MoviesNewForm";
 
 function App() {
@@ -17,16 +23,21 @@ function App() {
     <div className="wrapper">
       <Router>
         <Nav />
+
         <Routes>
           <Route path="/" element={<Home />} />
+
           <Route path="/shows" element={<ShowsIndex />} />
           <Route path="/shows/new" element={<ShowsNewForm />} />
           <Route path="/shows/:id" element={<Show />} />
           <Route path="/shows/:id/edit" element={<ShowsEditForm />} />
+
           <Route path="/movies" element={<MoviesIndex />} />
           <Route path="/movies/:id" element={<Movie />} />
           <Route path="/movies/new" element={<MoviesNewForm />} />
+          <Route path="/movies/:id/edit" element={<MoviesEditForm />} />
         </Routes>
+
         <Footer />
       </Router>
     </div>
