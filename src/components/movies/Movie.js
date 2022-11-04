e9cba787e502dd0fd04144958f1023e1d83d5573
Lennie Nurse <lnursej@gmail.com>
2022-11-04 09:44:19 -0400

    "update"

---
+++ src/components/movies/Movie.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { useState, useEffect } from "react"
+
+function Movie(prop) {
+  return <div></div>
+}
+
+export default Movie
--- src/components/movies/Movies.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-function Movies(prop) {
-  return <div></div>
-}
-
-export default Movies
