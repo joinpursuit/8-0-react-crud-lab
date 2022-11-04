5448a1f1c1de9df7687cf023d7ac005c3567227a
bowersjuan <bowersjuan@gmail.com>
2022-11-03 20:53:06 -0400

    Delete helpers

--- src/helpers/formHelpers.js
+++ src/helpers/formHelpers.js
@@ -1,13 +1,1 @@
-/**
- * Saves text input for search in state
- * @param {str} e
- */
-function handleTextChange(e) {
-  const title = e.target.value;
-  const result = title.length ? filterShows(title, allShows) : allShows;
 
-  setSearchTitle(title);
-  setShows(result);
-}
-
-export { handleTextChange };
