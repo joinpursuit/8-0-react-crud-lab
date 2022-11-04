7229b684fac1dab8a6c9110fff016c469ef8fdad
bowersjuan <bowersjuan@gmail.com>
2022-11-03 20:53:42 -0400

    Update MovieIndex and Movie Listing

--- src/components/movies/MoviesIndex.js
+++ src/components/movies/MoviesIndex.js
@@ -1,11 +1,32 @@
 import { useState, useEffect } from "react";
 import ErrorMessage from "../errors/ErrorMessage";
+import MovieListing from "./MoviesListing";
 
 import { getAllMovies } from "../../api/fetch";
+
+function filterMovies(search, movies) {
+  return movies.filter((movie) => {
+    return movie.title.toLowerCase().match(search.toLowerCase());
+  });
+}
 
 export default function MoviesIndex() {
   const [loadingError, setLoadingError] = useState();
   const [movies, setMovies] = useState([]);
+  const [allMovies, setAllMovies] = useState();
+  const [searchTitle, setSearchTitle] = useState("");
+
+  /**
+   * Saves text input for search in state
+   * @param {str} e
+   */
+  function handleTextChange(e) {
+    const title = e.target.value;
+    const result = title.length ? filterMovies(title, allMovies) : allMovies;
+
+    setSearchTitle(title);
+    setMovies(result);
+  }
 
   useEffect(() => {
     getAllMovies()
@@ -24,13 +45,26 @@ export default function MoviesIndex() {
       {loadingError ? (
         <ErrorMessage />
       ) : (
-        <section>
-          <h3>Amount: {movies.length}</h3>
-          {movies.map((movie) => (
-            <div style={{ textAlign: "center" }}>
-              <h4>{movie.title}</h4>
-            </div>
-          ))}
+        <section className="movies-index-wrapper">
+          <h2>All Movies</h2>
+          <button>
+            <Link to="/movies/new">Add a new movie</Link>
+          </button>
+          <br />
+          <label htmlFor="searchTitle">
+            Search Movies:
+            <input
+              type="text"
+              value={searchTitle}
+              id="searchTitle"
+              onChange={handleTextChange}
+            />
+          </label>
+          <section className="movies-index">
+            {movies.map((movie) => {
+              return <MovieListing movie={movie} key={movie.id} />;
+            })}
+          </section>
         </section>
       )}
     </div>
