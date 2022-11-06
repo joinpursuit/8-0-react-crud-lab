b491d4de54da3eaefc5ed9e1a852eb4700e9b109
DanielR2105 <rodriguez.daniel2105@gmail.com>
2022-11-06 01:44:03 -0500

    finished lab

--- src/components/movies/MoviesNewForm.js
+++ src/components/movies/MoviesNewForm.js
@@ -1,54 +1,48 @@
-import { useState, useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { getOneShow, updateShow } from "../../api/fetch";
+import { createMovie } from "../../api/fetch";
 
-import "./ShowsForm.css";
-
-export default function ShowsForm() {
-  const navigate = useNavigate()
-  const { id } = useParams()
+import "./MoviesForm.css"
 
 
-  const [show, setShow] = useState({
-    type: "",
-    title: "",
-    country: "",
-    dateAdded: "",
-    description: "",
-    duration: "",
-    listedIn: "",
-    rating: "",
-    releaseYear: "",
-  });
 
-  function handleSubmit(event) {
-    event.preventDefault()
-    updateShow(id, show )
-    .then(res => navigate(`/shows/${res.id}`))
-    .catch(err => console.log(err))
-  }
+export default function MoviesForm() {
+    let navigate = useNavigate()
 
-  function handleTextChange(event) {
-    setShow({
-      ...show,
-      [event.target.id]: event.target.value,
-    });
-  }
+    const [movie, setMovie] = useState({
+        type: "",
+        title: "",
+        country: "",
+        dateAdded: "",
+        description: "",
+        duration: "",
+        listedIn: "",
+        rating: "",
+        releaseYear: "",
+      });
 
+      function handleSubmit(event) {
+        event.preventDefault()
+        createMovie(movie)
+        .then(res => navigate(`/movies/${res.id}`))
+        .catch(err => console.log(err))
+      }
 
-  useEffect(() => {
-    getOneShow(id).then(res => setShow(res))
-    .catch(err => console.log(err))
-  }, [id])
+      function handleTextChange(event) {
+        setMovie({
+          ...movie,
+          [event.target.id]: event.target.value,
+        });
+      }
 
-  return (
-    <form onSubmit={handleSubmit}>
+      return (
+        <form onSubmit={handleSubmit}>
       <label htmlFor="title">Title:</label>
       <input
         type="text"
         id="title"
-        value={show.title}
+        value={movie.title}
         onChange={handleTextChange}
       />
 
@@ -56,7 +50,7 @@ export default function ShowsForm() {
       <input
         type="text"
         id="description"
-        value={show.description}
+        value={movie.description}
         onChange={handleTextChange}
       />
 
@@ -64,7 +58,7 @@ export default function ShowsForm() {
       <input
         type="text"
         id="type"
-        value={show.type}
+        value={movie.type}
         onChange={handleTextChange}
       />
 
@@ -72,7 +66,7 @@ export default function ShowsForm() {
       <input
         type="text"
         id="rating"
-        value={show.rating}
+        value={movie.rating}
         onChange={handleTextChange}
       />
 
@@ -80,7 +74,7 @@ export default function ShowsForm() {
       <input
         type="text"
         id="listedIn"
-        value={show.listedIn}
+        value={movie.listedIn}
         onChange={handleTextChange}
       />
 
@@ -88,7 +82,7 @@ export default function ShowsForm() {
       <input
         type="text"
         id="duration"
-        value={show.duration}
+        value={movie.duration}
         onChange={handleTextChange}
       />
 
@@ -96,7 +90,7 @@ export default function ShowsForm() {
       <input
         type="text"
         id="releaseYear"
-        value={show.releaseYear}
+        value={movie.releaseYear}
         onChange={handleTextChange}
       />
 
@@ -104,7 +98,7 @@ export default function ShowsForm() {
       <input
         type="text"
         id="country"
-        value={show.country}
+        value={movie.country}
         onChange={handleTextChange}
       />
 
@@ -112,7 +106,7 @@ export default function ShowsForm() {
       <input
         type="text"
         id="dateAdded"
-        value={show.dateAdded}
+        value={movie.dateAdded}
         onChange={handleTextChange}
       />
 
@@ -120,5 +114,5 @@ export default function ShowsForm() {
 
       <input type="submit" />
     </form>
-  );
+      )
 }
