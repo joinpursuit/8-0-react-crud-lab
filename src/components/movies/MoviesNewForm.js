bc40b537546eda547135c250f97334f1b63e02a6
Lennie Nurse <lnursej@gmail.com>
2022-11-07 10:51:30 -0500

    "update"

--- src/components/movies/MoviesNewForm.js
+++ src/components/movies/MoviesNewForm.js
@@ -29,48 +29,91 @@ function MoviesNewForm() {
     })
   }
   return (
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="type">
-          Type:
-          <input
-            type="text"
-            id="type"
-            value={movie.type}
-            onChange={handleTextChange}
-          />
-        </label>
-        <label htmlFor="title">
-          Title:
-          <input type="text" id="title" value={movie.title} onChange={handleTextChange}/>
-        </label>
-        <label htmlFor="description">
-          Description:
-          <input type="text" id="description" value={movie.description} onChange={handleTextChange}/>
-        </label>
-        <label htmlFor="country">
-          Country:
-          <input type="text" id="" value={movie.country} onChange={handleTextChange}/>
-        </label>
-        <label htmlFor="">
-      
-          <input type="text" id="" value={} onChange={handleTextChange}/>
-        </label>
-        <label htmlFor="">
-          <input type="text" id="" value={} onChange={handleTextChange}/>
-        </label>
-        <label htmlFor="">
-          <input type="text" id="" value={} onChange={handleTextChange}/>
-        </label>
-        <label htmlFor="">
-          <input type="text" id="" value={} onChange={handleTextChange}/>
-        </label>
-        <label htmlFor="">
-          <input type="text" id="" value={} onChange={handleTextChange}/>
-        </label>
-        <br/>
-         <input type="submit"/>
-      </form>
-    
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="type">
+        Type:
+        <input
+          type="text"
+          id="type"
+          value={movie.type}
+          onChange={handleTextChange}
+        />
+      </label>
+      <label htmlFor="title">
+        Title:
+        <input
+          type="text"
+          id="title"
+          value={movie.title}
+          onChange={handleTextChange}
+        />
+      </label>
+      <label htmlFor="description">
+        Description:
+        <input
+          type="text"
+          id="description"
+          value={movie.description}
+          onChange={handleTextChange}
+        />
+      </label>
+      <label htmlFor="country">
+        Country:
+        <input
+          type="text"
+          id=""
+          value={movie.country}
+          onChange={handleTextChange}
+        />
+      </label>
+      <label htmlFor="rating">
+        Rating:
+        <input
+          type="text"
+          id="rating"
+          value={movie.rating}
+          onChange={handleTextChange}
+        />
+      </label>
+      <label htmlFor="listed in">
+        Listed In:
+        <input
+          type="text"
+          id="listedIn"
+          value={movie.listenIn}
+          onChange={handleTextChange}
+        />
+      </label>
+      <label htmlFor="duration">
+        Duration:
+        <input
+          type="text"
+          id="duration"
+          value={movie.duration}
+          onChange={handleTextChange}
+        />
+      </label>
+      <label htmlFor="release year">
+        Release Year:
+        <input
+          type="text"
+          id="releaseYear"
+          value={movie.releaseYear}
+          onChange={handleTextChange}
+        />
+      </label>
+      <label htmlFor="dateAdded">
+        Date Added:
+        <input
+          type="text"
+          id="dateAdded"
+          value={movie.dateAdded}
+          onChange={handleTextChange}
+        />
+      </label>
+      <br />
+      <input type="submit" />
+    </form>
   )
 }
 
