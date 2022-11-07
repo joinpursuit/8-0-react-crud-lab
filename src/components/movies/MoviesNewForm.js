4ee7257a78ed614f1c8e45cef78d812651e76f93
Lennie Nurse <lnursej@gmail.com>
2022-11-07 10:37:22 -0500

    "update"

--- src/components/movies/MoviesNewForm.js
+++ src/components/movies/MoviesNewForm.js
@@ -1,7 +1,10 @@
 import React from "react"
 import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { createMovie } from "../../api/fetch"
 
 function MoviesNewForm() {
+  const navigate = useNavigate()
   const [movie, setMovie] = useState({
     type: "",
     title: "",
@@ -13,8 +16,48 @@ function MoviesNewForm() {
     rating: "",
     releaseYear: "",
   })
-
-  return <div></div>
+  function handleSubmit(e) {
+    e.preventDefault()
+    createMovie(movie)
+      .then((res) => navigate(`/movies/${res.id}`))
+      .catch((err) => console(err))
+  }
+  function handleTextChange(e) {
+    setMovie({
+      ...movie,
+      [e.target.id]: e.target.value,
+    })
+  }
+  return (
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="type">
+          Type:
+          <input
+            type="text"
+            id="type"
+            value={movie.type}
+            onChange={handleTextChange}
+          />
+        </label>
+        <label htmlFor="title">
+          Title:
+          <input type="text" id="title" value={movie.title} onChange={handleTextChange}/>
+        </label>
+        <label htmlFor="">
+          <input type="" id="" value={} onChange={}/>
+        </label>
+        <label htmlFor="">
+          <input type="" id="" value={} onChange={}/>
+        </label>
+        <label htmlFor="">
+          <input type="" id="" value={} onChange={}/>
+        </label>
+        <label htmlFor="">
+          <input type="" id="" value={} onChange={}/>
+        </label>
+      </form>
+    
+  )
 }
 
 export default MoviesNewForm
