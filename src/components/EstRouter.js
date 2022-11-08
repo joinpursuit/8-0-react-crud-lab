ad6fef73eda01fa639ddc6c24f9a82186bdb3ab5
DestinyJoyner <destinyjoyner@pursuit.org>
2022-11-08 17:48:23 -0500

    final look over lab done

--- src/components/EstRouter.js
+++ src/components/EstRouter.js
@@ -3,6 +3,7 @@ import App from '../App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 function EstRouter(props) {
+    // Wrap the entire App in Router component allows for components to return Route components
     return (
         <Router>
             <React.StrictMode>
