bb277cb0d2e98ca64abf0bb11ef6d25033708b13
cmm-7 <carlosmendoza@pursuit.org>
2022-11-09 18:48:14 -0500

    react-crud-lab complete

--- src/components/movies/MoviesNewForm.jsx
+++ src/components/movies/MoviesNewForm.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import { createShow } from "../../api/fetch";
+import { createMovie } from "../../api/fetch";
 import { useNavigate } from "react-router-dom";
 
-import "./ShowsForm.css";
+import "../shows/ShowListing.css";
 
-export default function ShowsForm() {
-  const [show, setShow] = useState({
+export default function MoviesForm() {
+  const [movie, setMovie] = useState({
     type: "",
     title: "",
     country: "",
@@ -22,9 +22,9 @@ export default function ShowsForm() {
   function handleSubmit(event) {
     event.preventDefault();
 
-    createShow(show)
+    createMovie(movie)
       .then((response) => {
-        navigate(`/shows/${response.id}`);
+        navigate(`/movies/${response.id}`);
       })
       .catch((error) => {
         console.error(error);
@@ -32,8 +32,8 @@ export default function ShowsForm() {
   }
 
   function handleTextChange(event) {
-    setShow({
-      ...show,
+    setMovie({
+      ...movie,
       [event.target.id]: event.target.value,
     });
   }
@@ -44,7 +44,7 @@ export default function ShowsForm() {
       <input
         type="text"
         id="title"
-        value={show.title}
+        value={movie.title}
         onChange={handleTextChange}
       />
 
@@ -52,7 +52,7 @@ export default function ShowsForm() {
       <input
         type="text"
         id="description"
-        value={show.description}
+        value={movie.description}
         onChange={handleTextChange}
       />
 
@@ -60,7 +60,7 @@ export default function ShowsForm() {
       <input
         type="text"
         id="type"
-        value={show.type}
+        value={movie.type}
         onChange={handleTextChange}
       />
 
@@ -68,7 +68,7 @@ export default function ShowsForm() {
       <input
         type="text"
         id="rating"
-        value={show.rating}
+        value={movie.rating}
         onChange={handleTextChange}
       />
 
@@ -76,7 +76,7 @@ export default function ShowsForm() {
       <input
         type="text"
         id="listedIn"
-        value={show.listedIn}
+        value={movie.listedIn}
         onChange={handleTextChange}
       />
 
@@ -84,7 +84,7 @@ export default function ShowsForm() {
       <input
         type="text"
         id="duration"
-        value={show.duration}
+        value={movie.duration}
         onChange={handleTextChange}
       />
 
@@ -92,7 +92,7 @@ export default function ShowsForm() {
       <input
         type="text"
         id="releaseYear"
-        value={show.releaseYear}
+        value={movie.releaseYear}
         onChange={handleTextChange}
       />
 
@@ -100,7 +100,7 @@ export default function ShowsForm() {
       <input
         type="text"
         id="country"
-        value={show.country}
+        value={movie.country}
         onChange={handleTextChange}
       />
 
@@ -108,7 +108,7 @@ export default function ShowsForm() {
       <input
         type="text"
         id="dateAdded"
-        value={show.dateAdded}
+        value={movie.dateAdded}
         onChange={handleTextChange}
       />
 
--- src/components/shows/ShowsIndex.js
+++ src/components/shows/ShowsIndex.js
@@ -1,13 +1,52 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+
+import { getAllShows } from "../../api/fetch";
+import ShowListing from "./ShowListing"
 
 import ErrorMessage from "../errors/ErrorMessage";
 
 import "./ShowsIndex.css";
 
 export default function ShowsIndex() {
+  const [LoadingError, setLoadingError] = useState(false);
+  const [shows, setShows] = useState([]); // displays collection of shows to the user 
+  const [allShows, setAllShows] = useState([]); // <- So we have access to all shows when we need to use them
+  const [searchTitle, setSearchTitle] = useState(""); 
+
+useEffect(() => {
+  getAllShows()
+    .then((response) => {
+      setAllShows(response);
+      setShows(response);
+      setLoadingError(false);
+    })
+    .catch((error) => {
+      console.error(error);
+      setLoadingError(true);
+    });
+}, []); // Be sure to always add the empty dependancies array 
+        // The empty dependencie array will cause the useEffect to run only when the component is initially mounted
+
+
+  function filterShows(search, shows){
+    return shows.filter(show => {
+      return show.title.toLowerCase().match(search.toLowerCase());
+    })
+  }
+
+  function handleTextChange(event) {
+    const title = event.target.value;
+    const result = title.length ? filterShows(title, allShows) : allShows;
+  
+    setSearchTitle(title);
+    setShows(result);
+  }
+  
+
   return (
     <div>
-      {false ? (
+      {LoadingError ? (
         <ErrorMessage />
       ) : (
         <section className="shows-index-wrapper">
@@ -20,13 +59,17 @@ export default function ShowsIndex() {
             Search Shows:
             <input
               type="text"
-              // value={searchTitle}
+              value={searchTitle}
               id="searchTitle"
-              // onChange={handleTextChange}
+              onChange={handleTextChange}
             />
           </label>
           <section className="shows-index">
-            {/* <!-- ShowListing components --> */}
+            {/* <!-- ShowListing components --> */
+              shows.map(show => {
+                return <ShowListing show={show} key={show.id} />
+              })
+            }
           </section>
         </section>
       )}
