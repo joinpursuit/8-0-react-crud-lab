90cf5d4b4c28bcac5c27bf69d3d866174c4eff3a
cjs2408 <carlossuero@pursuit.org>
2022-11-19 14:42:51 -0500

    Done

---
+++ src/components/movies/MoviesEditForm.js
@@ -0,0 +1,121 @@
+
+
+import React from 'react';
+
+import { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import { updateMovie, getOneMovie } from '../../api/fetch';
+
+
+
+function MoviesEditForm() {
+
+    let navigate = useNavigate()
+    const { id } = useParams()
+
+    const [movie, setMovie] = useState({
+        type: "",
+        title: "",
+        country: "",
+        dateAdded: "",
+        description: "",
+        duration: "",
+        listedIn: "",
+        rating: "",
+        releaseYear: "",
+    })
+
+    const handleTextChange = (e) => {
+        e.preventDefault()
+
+        setMovie({...movie, [e.target.id]: e.target.value})
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        updateMovie(id, movie)
+            .then(() => navigate(`/movies/${id}`))
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor='title'>Title: </label>
+            <input 
+                type="text"
+                id="title"
+                value={movie.title}
+                onChange={handleTextChange}
+            />
+
+            <label htmlFor="description">Description:</label>
+            <input
+                type="text"
+                id="description"
+                value={movie.description}
+                onChange={handleTextChange}
+            />
+
+            <label htmlFor="type">Type</label>
+            <input
+                type="text"
+                id="type"
+                value={movie.type}
+                onChange={handleTextChange}
+            />
+
+            <label htmlFor="rating">Rating:</label>
+            <input
+                type="text"
+                id="rating"
+                value={movie.rating}
+                onChange={handleTextChange}
+            />
+
+            <label htmlFor="listedIn">Listed in</label>
+            <input
+                type="text"
+                id="listedIn"
+                value={movie.listedIn}
+                onChange={handleTextChange}
+            />
+
+            <label htmlFor="duration">Duration</label>
+            <input
+                type="text"
+                id="duration"
+                value={movie.duration}
+                onChange={handleTextChange}
+            />
+
+            <label htmlFor="releaseYear">Release Year</label>
+            <input
+                type="text"
+                id="releaseYear"
+                value={movie.releaseYear}
+                onChange={handleTextChange}
+            />
+
+            <label htmlFor="country">Country</label>
+            <input
+                type="text"
+                id="country"
+                value={movie.country}
+                onChange={handleTextChange}
+            />
+
+            <label htmlFor="dateAdded">Date added:</label>
+            <input
+                type="text"
+                id="dateAdded"
+                value={movie.dateAdded}
+                onChange={handleTextChange}
+            />
+
+            <br />
+            <input type="submit" />
+        </form>
+    );
+}
+
+export default MoviesEditForm;
