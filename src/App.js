ebda26554b6172f2fd5d628dcba9a6a3c827385f
cjs2408 <carlossuero@pursuit.org>
2022-12-04 16:23:42 -0500

    almost forgot

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// Components
+
 import Footer from "./components/common/Footer";
 import Home from "./components/home/Home";
 import Nav from "./components/common/Nav";
-import Show from "./components/shows/Show";
-import ShowsEditForm from "./components/shows/ShowsEditForm";
 import ShowsIndex from "./components/shows/ShowsIndex";
 import ShowsNewForm from "./components/shows/ShowsNewForm";
+import Show from "./components/shows/Show";
+import ShowsEditForm from "./components/shows/ShowsEditForm";
+import MoviesIndex from "./components/movies/MoviesIndex"
+import MoviesNewForm from './components/movies/MoviesNewForm'
+import Movie from "./components/movies/Movie"
+import MoviesEditForm from "./components/movies/MoviesEditForm"
+
 
 function App() {
   return (
@@ -20,6 +25,10 @@ function App() {
           <Route path="/shows/new" element={<ShowsNewForm />} />
           <Route path="/shows/:id" element={<Show />} />
           <Route path="/shows/:id/edit" element={<ShowsEditForm />} />
+          <Route path="/movies" element={<MoviesIndex />} />
+          <Route path="/movies/new" element={<MoviesNewForm />} />
+          <Route path='/movies/:id' element={<Movie />} />
+          <Route path="/movies/:id/edit" element={<MoviesEditForm />} />
         </Routes>
         <Footer />
       </Router>
--- src/components/movies/MoviesEditForm.js
+++ src/components/movies/MoviesEditForm.js
@@ -1,121 +1,123 @@
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { updateMovie, getOneMovie } from "../../api/fetch";
+import "./MoviesForm.css";
 
+export default function MoviesForm() {
+  const navigate = useNavigate()
+  const {id} = useParams()
 
-import React from 'react';
+  const [movie, setMovie] = useState({
+    type: "",
+    title: "",
+    country: "",
+    dateAdded: "",
+    description: "",
+    duration: "",
+    listedIn: "",
+    rating: "",
+    releaseYear: "",
+  });
 
-import { useState, useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+  useEffect(()=> {
+    getOneMovie(id)
+    .then(res => setMovie(res))
+    .catch(error => console.log(error))
+  }, [id])
 
-import { updateMovie, getOneMovie } from '../../api/fetch';
-
-
-
-function MoviesEditForm() {
-
-    let navigate = useNavigate()
-    const { id } = useParams()
-
-    const [movie, setMovie] = useState({
-        type: "",
-        title: "",
-        country: "",
-        dateAdded: "",
-        description: "",
-        duration: "",
-        listedIn: "",
-        rating: "",
-        releaseYear: "",
+  function handleTextChange(e) {
+    setMovie({
+      ...movie,
+      [e.target.id]: e.target.value,
+    });
+  }
+  function handleSubmit(e) {
+    e.preventDefault()
+    updateMovie(id, movie)
+    .then(res => {
+      setMovie(res)
+      navigate(`/movies/${id}`)
     })
+    .catch(error => console.error(error))
+  }
 
-    const handleTextChange = (e) => {
-        e.preventDefault()
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="title">Title:</label>
+      <input
+        type="text"
+        id="title"
+        value={movie.title}
+        onChange={handleTextChange}
+      />
 
-        setMovie({...movie, [e.target.id]: e.target.value})
-    }
+      <label htmlFor="description">Description:</label>
+      <input
+        type="text"
+        id="description"
+        value={movie.description}
+        onChange={handleTextChange}
+      />
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
+      <label htmlFor="type">Type</label>
+      <input
+        type="text"
+        id="type"
+        value={movie.type}
+        onChange={handleTextChange}
+      />
 
-        updateMovie(id, movie)
-            .then(() => navigate(`/movies/${id}`))
-    }
-    return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor='title'>Title: </label>
-            <input 
-                type="text"
-                id="title"
-                value={movie.title}
-                onChange={handleTextChange}
-            />
+      <label htmlFor="rating">Rating:</label>
+      <input
+        type="text"
+        id="rating"
+        value={movie.rating}
+        onChange={handleTextChange}
+      />
 
-            <label htmlFor="description">Description:</label>
-            <input
-                type="text"
-                id="description"
-                value={movie.description}
-                onChange={handleTextChange}
-            />
+      <label htmlFor="listedIn">Listed in</label>
+      <input
+        type="text"
+        id="listedIn"
+        value={movie.listedIn}
+        onChange={handleTextChange}
+      />
 
-            <label htmlFor="type">Type</label>
-            <input
-                type="text"
-                id="type"
-                value={movie.type}
-                onChange={handleTextChange}
-            />
+      <label htmlFor="duration">Duration</label>
+      <input
+        type="text"
+        id="duration"
+        value={movie.duration}
+        onChange={handleTextChange}
+      />
 
-            <label htmlFor="rating">Rating:</label>
-            <input
-                type="text"
-                id="rating"
-                value={movie.rating}
-                onChange={handleTextChange}
-            />
+      <label htmlFor="releaseYear">Release Year</label>
+      <input
+        type="text"
+        id="releaseYear"
+        value={movie.releaseYear}
+        onChange={handleTextChange}
+      />
 
-            <label htmlFor="listedIn">Listed in</label>
-            <input
-                type="text"
-                id="listedIn"
-                value={movie.listedIn}
-                onChange={handleTextChange}
-            />
+      <label htmlFor="country">Country</label>
+      <input
+        type="text"
+        id="country"
+        value={movie.country}
+        onChange={handleTextChange}
+      />
 
-            <label htmlFor="duration">Duration</label>
-            <input
-                type="text"
-                id="duration"
-                value={movie.duration}
-                onChange={handleTextChange}
-            />
+      <label htmlFor="dateAdded">Date added:</label>
+      <input
+        type="text"
+        id="dateAdded"
+        value={movie.dateAdded}
+        onChange={handleTextChange}
+      />
 
-            <label htmlFor="releaseYear">Release Year</label>
-            <input
-                type="text"
-                id="releaseYear"
-                value={movie.releaseYear}
-                onChange={handleTextChange}
-            />
+      <br />
 
-            <label htmlFor="country">Country</label>
-            <input
-                type="text"
-                id="country"
-                value={movie.country}
-                onChange={handleTextChange}
-            />
-
-            <label htmlFor="dateAdded">Date added:</label>
-            <input
-                type="text"
-                id="dateAdded"
-                value={movie.dateAdded}
-                onChange={handleTextChange}
-            />
-
-            <br />
-            <input type="submit" />
-        </form>
-    );
+      <input type="submit" />
+    </form>
+  );
 }
-
-export default MoviesEditForm;
